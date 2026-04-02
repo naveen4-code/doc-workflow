90bc7458c89195d9faf0686b030344d3276f696e
Naveen Raj K <knaveenraj725@gmail.com>
2026-04-02 09:04:40 +0530

    commit

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,33 @@
 async function signup() {
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
   const email = emailInput.value;
   const password = passwordInput.value;
+
   const { data, error } = await supabaseClient.auth.signUp({
     email,
     password
   });
-  if (error) return alert(error.message);
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
 
   await supabaseClient.from("users").insert({
     id: data.user.id,
     email,
     role: "user"
   });
-  alert("Signup success");
+
+  alert("Signup successful");
 }
+
 async function login() {
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
   const email = emailInput.value;
   const password = passwordInput.value;
 
@@ -22,14 +35,19 @@ async function login() {
     email,
     password
   });
-  if (error) return alert(error.message);
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
 
   const { data: user } = await supabaseClient
     .from("users")
     .select("*")
     .eq("id", data.user.id)
     .single();
-  if (user.role === "admin") location.href = "admin.html";
-  else if (user.role === "reviewer") location.href = "reviewer.html";
-  else location.href = "user.html";
+
+  if (user.role === "admin") window.location.href = "admin.html";
+  else if (user.role === "reviewer") window.location.href = "reviewer.html";
+  else window.location.href = "user.html";
 }
